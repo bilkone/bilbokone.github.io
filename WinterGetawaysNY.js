1263cfbded720615d6e0b3eef05d4b0619a01b9f
Billy <54683912+bilkone@users.noreply.github.com>
2019-12-20 12:58:40 -0500

    Add files via upload

---
+++ WinterGetawaysNY.js
@@ -0,0 +1,11 @@
+function process (){
+    
+    var ful_name = document.getElementById('full_name').value;
+    
+    var result = '';
+        
+    result += 'Full Name: ' + fullname + '<br>';
+    
+    document.getElementById('result').innerHTML = result;
+    
+}
---
+++ first.js
@@ -0,0 +1,1 @@
+    $(".animated").addClass("delay-4s");
